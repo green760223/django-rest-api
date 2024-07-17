2d31ccba95260f3a13ee461642118e8258923b06
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-07-17 02:46:12 -0700

    Update api path

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from myapp.views import LoginView, RegisterView, UserListView
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("users/", UserListView.as_view(), name="user-list"),
+    path("register", RegisterView.as_view(), name="register"),
+    path("login", LoginView.as_view(), name="login"),
+    path("users", UserListView.as_view(), name="user-list"),
 ]
