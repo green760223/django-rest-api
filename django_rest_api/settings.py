4d56b760f7bde1d1c94c90bfb96b0f8639c9edda
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-07-17 03:22:19 -0700

    Update config

--- django_rest_api/settings.py
+++ django_rest_api/settings.py
@@ -132,8 +132,8 @@ USE_I18N = True
 USE_TZ = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5176",
-    "http://localhost:5176",
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
     "https://django-rest-api-ugje.onrender.com",
 ]
 
