d1813cdbb8d1bf6056cbf35b1427c4b620735804
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-07-19 03:03:32 -0700

    Update files

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -28,4 +28,4 @@ class LoginSerializer(serializers.Serializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email", "firstname", "lastname"]
+        fields = ["id", "username", "email", "first_name", "last_name"]
