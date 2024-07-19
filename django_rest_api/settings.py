011752f1d885fd43326ce0aae9080b564d685414
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-07-18 21:17:26 -0700

    Update files

--- django_rest_api/settings.py
+++ django_rest_api/settings.py
@@ -61,7 +61,10 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = "django_rest_api.urls"
 
